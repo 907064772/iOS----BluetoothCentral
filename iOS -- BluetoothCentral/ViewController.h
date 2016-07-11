e0bac207caed03268bcd4395f4721b1dd74015de
GIT <907064772@qq.com>
2016-07-11 09:27:21 +0800

    Initial Commit

---
+++ iOS -- BluetoothCentral/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS -- BluetoothCentral
+//
+//  Created by luodp on 16/7/11.
+//  Copyright © 2016年 zhanghao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOS -- BluetoothCentral/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iOS -- BluetoothCentral
+//
+//  Created by luodp on 16/7/11.
+//  Copyright © 2016年 zhanghao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
